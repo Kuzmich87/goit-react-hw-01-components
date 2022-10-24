f79ee4d43a1c6f140962a6f6a93f7d6f99b12b76
Kuzmich87 <d.kuzmich87@gmail.com>
2022-10-24 20:40:06 +0300

    add 1-3

---
+++ src/components/Friends/FriendList/FriendList.styled.js
@@ -0,0 +1,10 @@
+//import styled from '@emotion/styled'
+import styled from 'styled-components';
+
+export const OurFriends = styled.ul`
+  width: 400px;
+  margin: 40px;
+  >:nth-child(n+2){
+    margin-top: 20px;
+  }
+`;
